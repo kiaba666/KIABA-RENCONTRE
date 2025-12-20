aec21b9e34bd5f1222aa6db1884375621b35d620
Chaka06 <diarrassoubaissiaka662@gmail.com>
2025-12-20 21:01:55 +0000

    Add: Migration pour mettre à jour l'ordering du modèle Ad avec les champs boost

---
+++ ads/migrations/0005_alter_ad_ordering.py
@@ -0,0 +1,18 @@
+# Generated manually for Ad model ordering update
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ads', '0004_ad_boost_fields'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='ad',
+            options={'ordering': ['-is_premium', '-is_urgent', '-created_at']},
+        ),
+    ]
+
